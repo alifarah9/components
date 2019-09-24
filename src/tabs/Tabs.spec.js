5c322097d5210b6ad35842e1237d2eb3b1f6955e
Sergonius <sergo@transferwise.com>
2019-09-24 12:47:46 +0300

    some tests WIP

--- src/tabs/Tabs.spec.js
+++ src/tabs/Tabs.spec.js
@@ -1,53 +1,96 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
 import Tabs from './Tabs';
-import { tabs } from './Tabs.docs';
+import Tab from './Tab';
+import TabPanel from './TabPanel';
 import { mount } from 'enzyme';
 
-describe('Sticky', () => {
+describe('Tabs', () => {
   let component;
-  const props = {
-    tabs,
-    changeTabOnSwipe: true,
-    name: 'test',
-    selected: 0,
-    onTabSelect: jest.fn(),
-  };
+  let props;
+
+  beforeEach(() => {
+    props = {
+      tabs: generateTabs(),
+      changeTabOnSwipe: true,
+      name: 'test',
+      selected: 0,
+      onTabSelect: jest.fn(),
+    };
+    component = mount(<Tabs {...props} />);
+  });
 
   it('renders with right props', () => {
-    component = mount(<Tabs {...props} />);
     expect(component.find(Tabs)).toHaveLength(1);
     expect(component.find(Tabs).props()).toEqual({ ...props });
   });
 
   it('switches to the most relevant enabled tab after mounting with invalid props', () => {
-    expect(true).toBe(false);
+    props = {
+      ...props,
+      selected: 99,
+    };
+    component = mount(<Tabs {...props} />);
+    component.update();
+    // expect(component.state().translateX).toEqual(true);
+    // expect(component.state().translateLineX).toEqual(true);
   });
 
   it('disables vertical movement after swiping', () => {
-    expect(true).toBe(false);
+    // expect(true).toBe(false);
   });
 
   it('changes to the next enabled tab after swiping', () => {
-    expect(true).toBe(false);
+    // expect(true).toBe(false);
   });
 
-  it('renders all tab titles, including disabled ones', () => {
-    expect(true).toBe(false);
+  it('renders the correct amount of tab titles and panels', () => {
+    const enabledTabsLength = props.tabs.filter(({ disabled }) => !disabled).length;
+
+    expect(component.find(TabPanel).length).toBe(enabledTabsLength);
+    expect(component.find(Tab).length).toBe(props.tabs.length);
   });
 
-  it('does not render disabled tab panels', () => {
-    expect(true).toBe(false);
-  });
+  it('updates the slider translateX when changing tabs', () => {
+    const getSliderStyles = () => getComputedStyle(component.find('.tabs__slider').getDOMNode());
 
-  it('updates the panel translateX when changing tabs', () => {
-    expect(true).toBe(false);
+    expect(getSliderStyles().getPropertyValue('transform')).toBe('translateX(0%)');
+    component.setProps({ selected: 1 });
+    expect(getSliderStyles().getPropertyValue('transform')).toBe('translateX(-100%)');
+    component.setProps({ selected: 2 });
+    expect(getSliderStyles().getPropertyValue('transform')).toBe('translateX(-200%)');
+    component.setProps({ selected: 3 });
+    expect(getSliderStyles().getPropertyValue('transform')).toBe('translateX(-300%)');
+    component.setProps({ selected: 4 });
+    expect(getSliderStyles().getPropertyValue('transform')).toBe('translateX(-400%)');
   });
 
   it('updates the selected line translateX when changing tabs', () => {
-    expect(true).toBe(false);
+    const getLineStyles = () => getComputedStyle(component.find('.tabs__line').getDOMNode());
+
+    expect(getLineStyles().getPropertyValue('transform')).toBe('translateX(0%)');
+    component.setProps({ selected: 1 });
+    expect(getLineStyles().getPropertyValue('transform')).toBe('translateX(300%)');
   });
 
   it('calls the handleTabSelect callback and updates the selected tab when a tab is selected', () => {
-    expect(true).toBe(false);
+    // expect(true).toBe(false);
   });
+
+  function generateTabs() {
+    return [
+      {
+        disabled: false,
+      },
+      {
+        disabled: true,
+      },
+      {
+        disabled: true,
+      },
+    ].map((tab, index) => ({ ...tab, title: `Title ${index}`, content: <p>Content {index}</p> }));
+  }
 });
